7bf45684c38088bb4bd2276632a4e3463029a70d
Vlad <117743206+Wr1ter12@users.noreply.github.com>
2022-11-19 18:15:58 +0300

    Добавил более реалистичные размеры и расстояние от солнца, а также кручение вокруг солнца

--- Assets/Scripts/FlyCamera.cs
+++ Assets/Scripts/FlyCamera.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class FlyCamera : MonoBehaviour
 {
-    float mainSpeed = 100.0f; //regular speed
-    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    float camSens = 0.25f; //How sensitive it with mouse
-    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+    public float mainSpeed = 100.0f; 
+    public float shiftAdd = 250.0f; 
+    float maxShift = 5000.0f; 
+    public float camSens = 0.25f; 
+    private Vector3 lastMouse = new Vector3(255, 255, 255);
     private float totalRun= 1.0f;
      
     void Update () {
